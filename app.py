0f8bb5f131f853ed3cc8c58a5134767329945536
Carlos Escalante <cescalante2988@gmail.com>
2024-04-17 21:14:11 -0600

    Create pydantic validator for stocks

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request
 from markupsafe import escape
+from pydantic import BaseModel, field_validator
 
 app = Flask(__name__)
+
+
+class StockModel(BaseModel):
+    """Class for parsing new stock data from a form."""
+
+    stock_symbol: str
+    number_of_shares: int
+    purchase_price: float
+
+    @field_validator("stock_symbol")
+    def stock_symbol_check(cls, value):
+        if not value.isalpha() or len(value) > 5:
+            raise ValueError("Stock symbol must be 1-5 characters.")
+        return value.upper()
 
 
 @app.route("/")
